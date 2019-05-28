d6e7d82dceb275b49c6dedd4896bb4cfb34b4e38
TeodorPloae <ploaetdr@yahoo.com>
2019-05-29 00:48:55 +0300

    New Version of Aapp

--- ANEO_Banking/ANEO_Banking/Controllers/HomeController.cs
+++ ANEO_Banking/ANEO_Banking/Controllers/HomeController.cs
@@ -7,36 +7,92 @@ using ANEO_Banking.Models;
 
 namespace ANEO_Banking.Controllers
 {
+
+    
+    [Route("")]
     public class HomeController : Controller
     {
 
+
         Entities _db;
+
+        public class UserCredentials
+        {
+            public string cardNumber { get; set; }
+            public string cvv { get; set; }
+        }
+
+
 
         public HomeController()
         {
             _db = new Entities();
 
         }
-        
+
         public ActionResult Index()
         {
-            var altceva = _db.PACKS.ToList();
-            Console.Write(altceva);
-            return View();
-        }
+            if (Session["UserCardNumber"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
 
-        public ActionResult About()
+            
+        }
+        
+        public ActionResult PACKS()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Packs";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult TR_HIST()
         {
-            ViewBag.Message = "Your contact page.";
+
+            string cardNo = Session["UserCardNumber"].ToString();
+            decimal id_cust = _db.CUSTOMER_CREDIT_CARDS.Where(c => c.CARD_NUMBER == cardNo).FirstOrDefault().ID;
+            string prenume = _db.CUSTOMERS.Where(i => i.ID == id_cust).FirstOrDefault().PRENUME;
+            ViewBag.Message = "Hello " + prenume;
+
+            string account_number = _db.CUSTOMER_CREDIT_CARDS.Where(c => c.CARD_NUMBER == cardNo).FirstOrDefault().ACCOUNT_NUMBER;
+            List<decimal> ids = _db.TRANSACTIONS.Where(a => a.ACC_NO_CUSTOMER1 == account_number || a.ACC_NO_CUSTOMER2 == account_number).Select(a => a.ID).ToList();
+            List<ANEO_Banking.Models.TRANSACTIONS_HISTORY> transaction_details = _db.TRANSACTIONS_HISTORY.Where(i => ids.Contains(i.ID_TRANSACTION)).Select(x => x).ToList();
+            ViewBag.Data = transaction_details;
 
             return View();
         }
+        
+            public ActionResult Login()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Login(CREDIT_CARDS objUser)
+            {
+                if (ModelState.IsValid)
+                {
+                    using (_db )
+                    {
+                        var obj = _db.CREDIT_CARDS.Where(a => a.CARD_NUMBER.Equals(objUser.CARD_NUMBER) && a.CVV.Equals(objUser.CVV)).FirstOrDefault();
+                        if (obj != null)
+                        {
+                            Session["UserCardNumber"] = obj.CARD_NUMBER.ToString();
+                            Session["UserCVV"] = obj.CVV.ToString();
+                            return RedirectToAction("Index");
+                        }
+                    }
+                }
+                return View(objUser);
+            }
+
+            
+        
     }
 }
